7a3675237a7392c3d4f173a6b27b82c8fbf0664c
faruqhossen30 <faruqhossen30@gmail.com>
2024-01-10 12:35:20 +0600

    homepage some section added

---
+++ resources/views/inc/homepage/blog.blade.php
@@ -0,0 +1,44 @@
+<div class="container pb-20">
+    <p class=" text-center text-red-500 text-base uppercase">Letest news</p>
+    <h1 class="text-3xl text-center mt-3 mb-10">From The Blogs</h1>
+
+    <div class="grid grid-cols-12 gap-7">
+
+        <div class=" col-span-4 space-y-3 group ">
+            <a href="#" class=" overflow-hidden block ">
+                <img class=" group-hover:scale-110 transition duration-500" src="{{ asset('img/Blog/blog-1.jpg') }}" alt="">
+            </a>
+            <div class=" space-y-2">
+                <p class="italic font-semibold text-gray-600">May 15, 2023</p>
+                <a href=""><h2 class=" text-2xl font-[500] tracking-wider">Blog Elegance: Your Style, Your...</h2></a>
+                <p class=" text-gray-500">Blog Elegance is your virtual runway for all things bag fashion. From timeless classics to letest</p>
+                <a class=" hover:text-red-500 transition cursor-pointer"><span><i class="fa-solid fa-minus "></i></span> Read more</a>
+            </div>
+        </div>
+
+        <div class=" col-span-4 space-y-3 group ">
+            <a href="#" class=" overflow-hidden block ">
+                <img class=" group-hover:scale-110 transition duration-500" src="{{ asset('img/Blog/blog-2.jpg') }}" alt="">
+            </a>
+            <div class=" space-y-2">
+                <p class="italic font-semibold text-gray-600">May 15, 2023</p>
+                <a href=""><h2 class=" text-2xl font-[500] tracking-wider">Blog Elegance: Your Style, Your...</h2></a>
+                <p class=" text-gray-500">Blog Elegance is your virtual runway for all things bag fashion. From timeless classics to letest</p>
+                <a class=" hover:text-red-500 transition cursor-pointer"><span><i class="fa-solid fa-minus "></i></span> Read more</a>
+            </div>
+        </div>
+
+        <div class=" col-span-4 space-y-3 group ">
+            <a href="#" class=" overflow-hidden block ">
+                <img class=" group-hover:scale-110 transition duration-500" src="{{ asset('img/Blog/blog-3.jpg') }}" alt="">
+            </a>
+            <div class=" space-y-2">
+                <p class="italic font-semibold text-gray-600">May 15, 2023</p>
+                <a href=""><h2 class=" text-2xl font-[500] tracking-wider">Blog Elegance: Your Style, Your...</h2></a>
+                <p class=" text-gray-500">Blog Elegance is your virtual runway for all things bag fashion. From timeless classics to letest</p>
+                <a class=" hover:text-red-500 transition cursor-pointer"><span><i class="fa-solid fa-minus "></i></span> Read more</a>
+            </div>
+        </div>
+
+    </div>
+</div>
---
+++ resources/views/inc/homepage/offerproduct.blade.php
@@ -0,0 +1,193 @@
+<div class="container py-12 grid grid-cols-12  gap-7">
+
+    <div class=" col-span-12 lg:col-span-4 space-y-6">
+        <div href="" class=" relative group cursor-pointer">
+            <div
+                class=" h-full w-full bg-gray-100 absolute left-0 top-0 invisible group-hover:visible bg-opacity-75 flex items-center justify-center">
+                <button
+                    class="bg-black rounded-full text-white py-3 px-7 text-xl font-semibold scale-0 group-hover:scale-100 transition duration-500 hover:bg-red-500">View
+                    Products</button>
+            </div>
+            <div class=" flex flex-col lg:flex-row items-center justify-center bg-gray-100 py-10  md:p-4 space-x-2">
+                <div class="text-center lg:text-left order-2 lg:order-1">
+                    <h2 class=" text-lg lg:text-xl font-bold">Leather Shoes</h2>
+                    <p><s class="text-gray-500">20$</s> <span class=" text-red-500 text-lg font-semibold">$18.00</span>
+                    </p>
+                </div>
+                <div class="order-1 lg:order-2">
+                    <img class=" max-h-44" src="{{ asset('img/banner-sale-01.png') }}" alt="Images">
+                </div>
+            </div>
+            <div class="bg-red-500 mt-3 text-white py-2 flex justify-around">
+                <div class="text-center leading-5">
+                    <h2 class="text-xl leading-5">356</h2>
+                    <p>Days</p>
+                </div>
+                <div class="text-center leading-5">
+                    <h2 class="text-xl leading-5">23</h2>
+                    <p>Hours</p>
+                </div>
+                <div class="text-center leading-5">
+                    <h2 class="text-xl leading-5">34</h2>
+                    <p>Mins</p>
+                </div>
+                <div class="text-center leading-5">
+                    <h2 class="text-xl leading-5">14</h2>
+                    <p>Sec</p>
+                </div>
+            </div>
+        </div>
+        <div href="" class=" relative group cursor-pointer">
+            <div
+                class=" h-full w-full bg-gray-100 absolute left-0 top-0 invisible group-hover:visible bg-opacity-75 flex items-center justify-center">
+                <button
+                    class="bg-black rounded-full text-white py-3 px-7 text-xl font-semibold scale-0 group-hover:scale-100 transition duration-500 hover:bg-red-500">View
+                    Products</button>
+            </div>
+            <div class=" flex flex-col lg:flex-row items-center justify-center bg-gray-100 py-10  md:p-4 space-x-2">
+                <div class="text-center lg:text-left order-2 lg:order-1">
+                    <h2 class=" text-lg lg:text-xl font-bold">Leather Shoes</h2>
+                    <p><s class="text-gray-500">20$</s> <span class=" text-red-500 text-lg font-semibold">$18.00</span>
+                    </p>
+                </div>
+                <div class="order-1 lg:order-2">
+                    <img class=" max-h-44" src="{{ asset('img/banner-sale-04.png') }}" alt="Images">
+                </div>
+            </div>
+            <div class="bg-red-500 mt-3 text-white py-2 flex justify-around">
+                <div class="text-center leading-5">
+                    <h2 class="text-xl leading-5">356</h2>
+                    <p>Days</p>
+                </div>
+                <div class="text-center leading-5">
+                    <h2 class="text-xl leading-5">23</h2>
+                    <p>Hours</p>
+                </div>
+                <div class="text-center leading-5">
+                    <h2 class="text-xl leading-5">34</h2>
+                    <p>Mins</p>
+                </div>
+                <div class="text-center leading-5">
+                    <h2 class="text-xl leading-5">14</h2>
+                    <p>Sec</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class=" col-span-12 lg:col-span-4 space-y-6">
+        <div  class="flex flex-col justify-between h-full relative group cursor-pointer">
+            <div
+                class=" h-full w-full bg-gray-100 absolute left-0 top-0 invisible group-hover:visible bg-opacity-75 flex items-center justify-center">
+                <button
+                    class="bg-black rounded-full text-white py-3 px-7 text-xl font-semibold scale-0 group-hover:scale-100 transition duration-500 hover:bg-red-500">View
+                    Products</button>
+            </div>
+
+            <div class="bg-gray-100 py-10 h-full md:p-4 space-x-2">
+
+                <div class="order-1 text-center lg:order-2">
+                    <img class="mx-auto block" src="{{ asset('img/banner-sale-05.png') }}" alt="Images">
+                </div>
+                <div class="text-center lg:text-left order-2 lg:order-1">
+                    <h2 class=" text-center text-lg lg:text-xl font-bold">Leather Shoes</h2>
+                    <p class="text-center"><s class="text-gray-500">20$</s> <span class=" text-red-500 text-lg font-semibold">$18.00</span>
+                    </p>
+                </div>
+            </div>
+            <div class="bg-red-500 mt-3 text-white py-2 flex justify-around">
+                <div class="text-center leading-5">
+                    <h2 class="text-xl leading-5">356</h2>
+                    <p>Days</p>
+                </div>
+                <div class="text-center leading-5">
+                    <h2 class="text-xl leading-5">23</h2>
+                    <p>Hours</p>
+                </div>
+                <div class="text-center leading-5">
+                    <h2 class="text-xl leading-5">34</h2>
+                    <p>Mins</p>
+                </div>
+                <div class="text-center leading-5">
+                    <h2 class="text-xl leading-5">14</h2>
+                    <p>Sec</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class=" col-span-12 lg:col-span-4 space-y-6">
+        <div href="" class=" relative group cursor-pointer">
+            <div
+                class=" h-full w-full bg-gray-100 absolute left-0 top-0 invisible group-hover:visible bg-opacity-75 flex items-center justify-center">
+                <button
+                    class="bg-black rounded-full text-white py-3 px-7 text-xl font-semibold scale-0 group-hover:scale-100 transition duration-500 hover:bg-red-500">View
+                    Products</button>
+            </div>
+            <div class=" flex flex-col lg:flex-row items-center justify-center bg-gray-100 py-10  md:p-4 space-x-2">
+                <div class="text-center lg:text-left order-2 lg:order-1">
+                    <h2 class=" text-lg lg:text-xl font-bold">Leather Shoes</h2>
+                    <p><s class="text-gray-500">20$</s> <span class=" text-red-500 text-lg font-semibold">$18.00</span>
+                    </p>
+                </div>
+                <div class="order-1 lg:order-2">
+                    <img class=" max-h-44" src="{{ asset('img/banner-sale-01.png') }}" alt="Images">
+                </div>
+            </div>
+            <div class="bg-red-500 mt-3 text-white py-2 flex justify-around">
+                <div class="text-center leading-5">
+                    <h2 class="text-xl leading-5">356</h2>
+                    <p>Days</p>
+                </div>
+                <div class="text-center leading-5">
+                    <h2 class="text-xl leading-5">23</h2>
+                    <p>Hours</p>
+                </div>
+                <div class="text-center leading-5">
+                    <h2 class="text-xl leading-5">34</h2>
+                    <p>Mins</p>
+                </div>
+                <div class="text-center leading-5">
+                    <h2 class="text-xl leading-5">14</h2>
+                    <p>Sec</p>
+                </div>
+            </div>
+        </div>
+        <div href="" class=" relative group cursor-pointer">
+            <div
+                class=" h-full w-full bg-gray-100 absolute left-0 top-0 invisible group-hover:visible bg-opacity-75 flex items-center justify-center">
+                <button
+                    class="bg-black rounded-full text-white py-3 px-7 text-xl font-semibold scale-0 group-hover:scale-100 transition duration-500 hover:bg-red-500">View
+                    Products</button>
+            </div>
+            <div class=" flex flex-col lg:flex-row items-center justify-center bg-gray-100 py-10  md:p-4 space-x-2">
+                <div class="text-center lg:text-left order-2 lg:order-1">
+                    <h2 class=" text-lg lg:text-xl font-bold">Leather Shoes</h2>
+                    <p><s class="text-gray-500">20$</s> <span class=" text-red-500 text-lg font-semibold">$18.00</span>
+                    </p>
+                </div>
+                <div class="order-1 lg:order-2">
+                    <img class=" max-h-44" src="{{ asset('img/banner-sale-03.png') }}" alt="Images">
+                </div>
+            </div>
+            <div class="bg-red-500 mt-3 text-white py-2 flex justify-around">
+                <div class="text-center leading-5">
+                    <h2 class="text-xl leading-5">356</h2>
+                    <p>Days</p>
+                </div>
+                <div class="text-center leading-5">
+                    <h2 class="text-xl leading-5">23</h2>
+                    <p>Hours</p>
+                </div>
+                <div class="text-center leading-5">
+                    <h2 class="text-xl leading-5">34</h2>
+                    <p>Mins</p>
+                </div>
+                <div class="text-center leading-5">
+                    <h2 class="text-xl leading-5">14</h2>
+                    <p>Sec</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
